17057d3b53b38c33dcd9f3a217885d66f55743c2
thruc <horiuchi.github@gmail.com>
2021-01-23 21:38:56 +0900

    useEffect 条件付き

--- src/component/BasicUseEffect.js
+++ src/component/BasicUseEffect.js
@@ -6,17 +6,20 @@ const BasicUseEffect = () => {
 
     const [count, setCount ] = useState(0)
 
+    const [item, setItem] = useState('')
+
     useEffect(() => {//レンダリングが実行されるたびに実行される
         console.log("use Effect")
         // effect
         // return () => {
         //     cleanup
         // }
-    },[])
+    },[count])
 
     return (
         <>
             <button onClick={()=> setCount(prevCount=>prevCount+1)}>click {count}</button>
+            <input type='text' value={item} onChange={evt=>setItem(evt.target.value)}/>
             
         </>
     )
