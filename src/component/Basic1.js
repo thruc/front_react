a035788fe3774e38883ce8794c4bafb9a5bd837a
thruc <horiuchi.github@gmail.com>
2021-01-23 16:30:11 +0900

    Handler

--- src/component/Basic1.js
+++ src/component/Basic1.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Basic1 = (props) => {
+    const clickHandler = () => {
+        console.log('clicked')
+    }
     return (
-        <>
+        <>  
+            <button onClick={clickHandler}>Click</button>
             <h1>hello { props.name }</h1>
             
         </>
