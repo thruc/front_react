85286064beba90f176587f866207a429975a49d7
thruc <horiuchi.github@gmail.com>
2021-01-23 16:25:19 +0900

    props

--- src/component/Basic1.js
+++ src/component/Basic1.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Basic1 = () => {
+const Basic1 = (props) => {
     return (
         <>
-            <h1>hello react</h1>
+            <h1>hello { props.name }</h1>
             
         </>
     )
