6a89aa12721806406360f09526cb0e38ba5cf352
thruc <horiuchi.github@gmail.com>
2021-01-30 13:41:24 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // import Basic2 from './component/Basic2'
-import BasicUseEffect from './component/BasicUseEffect'
-import TimerContainer from './component/TimerContainer'
-import ApiFetch from './component/ApiFetch'
+import BasicUseEffect from "./component/BasicUseEffect";
+import TimerContainer from "./component/TimerContainer";
+import ApiFetch from "./component/ApiFetch";
+import AppContext from "./contexts/AppContext";
+import B from "./component/B";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic2 /> */}
-        {/* <BasicUseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-      </header>
-    </div>
-  );
+    return (
+        <AppContext.Provider value={"value from App.js"}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    {/* <Basic2 /> */}
+                    {/* <BasicUseEffect /> */}
+                    {/* <TimerContainer /> */}
+                    <ApiFetch />
+                    <B />
+                </header>
+            </div>
+        </AppContext.Provider>
+    );
 }
 
 export default App;
-
