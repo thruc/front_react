ed40a8142e904b08995f8c3c40fabe73ddfa333a
thruc <horiuchi.github@gmail.com>
2021-01-30 13:19:27 +0900

    fetch

--- src/component/ApiFetch.js
+++ src/component/ApiFetch.js
@@ -1,10 +1,15 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ApiFetch = () => {
+    const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(1);
+    const [clicked, setClicked] = useState(false); //トグル
 
-    const [posts, setPosts] = useState([])
+    const handlerClicked = () => {
+        setClicked(!clicked); //ture false 反転
+    };
+
     useEffect(() => {
         // axios.get('https://jsonplaceholder.typicode.com/posts''https://jsonplaceholder.typicode.com/posts')
         // .then(res =>{
@@ -12,23 +17,32 @@ const ApiFetch = () => {
         // })
         // return () => {
         // }
-        fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
-        .then(res => res.json())
-        .then(data => {
-            setPosts(data)
-        })
-    }, [])
+
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: "GET" })
+            .then((res) => res.json())
+            .then((data) => {
+                setPosts(data);
+            });
+    }, [clicked]);
 
     return (
         <div>
-            <ul>
+            <input type="text" value={id} onChange={(evt) => setId(evt.target.value)} />
+            <br/>
+            <button type="button" onClick={handlerClicked}>
+                Get Post
+            </button>
+            <br/>
+
+            { posts.title }
+            {/* <ul>
                 {
                     posts.map(post => <li key={post.id}>{post.title}</li>)
                 }
-            </ul>
-            
-        </div>
-    )
-}
 
-export default ApiFetch
+            </ul> */}
+        </div>
+    );
+};
+
+export default ApiFetch;
